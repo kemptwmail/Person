89254b1c4951842fd22647252732d41b6269fe16
kemptwmail <kemptw@mail.uc.edu>
2023-09-12 22:00:17 -0400

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,77 @@
+public class Person
+{
+   private String ID;
+   private String firstName;
+   private String lastName;
+   private String title;
+   private int YOB;
+
+    public Person(String ID, String firstName, String lastName, String title, int YOB)
+    {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.YOB = YOB;
+    }
+    public Person(){}
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+    public String fullName(){
+        return firstName+ " "+lastName;
+    }
+    public String formalName(){
+        return title+ " "+(firstName+" "+lastName);
+    }
+    public String getAge() {
+        return String.valueOf((2023 - YOB));
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ID='" + ID + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", title='" + title + '\'' +
+                ", YOB=" + YOB +
+                '}';
+    }
+}
---
+++ src/PersonTest.java
@@ -0,0 +1,11 @@
+
+public class PersonTest
+{
+    public static void main(String[] args)
+    {
+
+
+    }
+
+    
+}
